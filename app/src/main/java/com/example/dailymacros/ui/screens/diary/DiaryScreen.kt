b55020e50ad4948b9eb092523b9ecabb60ed8858
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2024-07-31 16:44:55 +0200

    DatePicker now in composables

--- app/src/main/java/com/example/dailymacros/ui/screens/diary/DiaryScreen.kt
+++ app/src/main/java/com/example/dailymacros/ui/screens/diary/DiaryScreen.kt
@@ -1,42 +1,13 @@
 package com.example.dailymacros.ui.screens.diary
 
-import android.text.format.DateUtils
-import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerDialog
-import androidx.compose.material3.DatePickerFormatter
-import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.dailymacros.ui.composables.DMTopAppBar
-import java.text.SimpleDateFormat
-import java.time.Instant
-import java.util.Date
-import java.util.Locale
+import com.example.dailymacros.ui.composables.datePickerWithDialog
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -44,110 +15,6 @@ fun Diary(navController: NavHostController) {
     Scaffold (
         topBar = { DMTopAppBar(navController) }
     ){paddingValues ->
-        //val dateState = rememberDatePickerState(initialDisplayMode = DisplayMode.Input)
-
-        //DatePicker(state = dateState, modifier = Modifier.padding(16.dp), dateFormatter = DatePickerFormatter())
-        DatePickerWithDialog(Modifier.padding(paddingValues))
-    }
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun DatePickerWithDialog(
-    modifier: Modifier = Modifier
-) {
-    var selectedDateMillis by remember { mutableStateOf<Long?>(Date().time) }
-    val dateState = rememberDatePickerState(initialSelectedDateMillis = selectedDateMillis)
-    Log.v("DiaryScreen", dateState.selectedDateMillis.toString())
-    var showDialog by remember { mutableStateOf(false) }
-    val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    color = MaterialTheme.colorScheme.tertiary,
-                    shape = RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp)
-                )
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Button(
-                onClick = {
-                    selectedDateMillis?.let {
-                        selectedDateMillis = it - DateUtils.DAY_IN_MILLIS
-                    }
-                },
-                enabled = selectedDateMillis != null,
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.tertiary,
-                    contentColor = MaterialTheme.colorScheme.onTertiary
-                )
-            ) {
-                Text("<",
-                    textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.headlineMedium)
-            }
-            Text(
-                text = selectedDateMillis?.let { dateFormat.format(Date(it)) } ?: dateFormat.format(Date()),
-                color = MaterialTheme.colorScheme.onTertiary,
-                modifier = Modifier
-                    .clickable(onClick = {
-                        showDialog = true
-                    })
-                    .padding(horizontal = 16.dp),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.headlineMedium
-            )
-            Button(
-                onClick = {
-                    selectedDateMillis?.let {
-                        selectedDateMillis = it + DateUtils.DAY_IN_MILLIS
-                    }
-                },
-                enabled = selectedDateMillis != null,
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.tertiary,
-                    contentColor = MaterialTheme.colorScheme.onTertiary
-                )
-            ) {
-                Text(">",
-                    textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.headlineMedium)
-            }
-        }
-        if (showDialog) {
-            DatePickerDialog(
-                onDismissRequest = { showDialog = false },
-                confirmButton = {
-                    Button(
-                        onClick = {
-                            selectedDateMillis = dateState.selectedDateMillis
-                            showDialog = false
-                        }
-                    ) {
-                        Text(text = "OK")
-                    }
-                },
-                dismissButton = {
-                    Button(
-                        onClick = { showDialog = false }
-                    ) {
-                        Text(text = "Cancel")
-                    }
-                }
-            ) {
-                DatePicker(
-                    state = dateState,
-                    showModeToggle = true
-                )
-            }
-        }
+        val dateState = datePickerWithDialog(Modifier.padding(paddingValues))
     }
 }
