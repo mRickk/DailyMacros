1eb887453b875467d84cb5fff655bf69ecdd4cad
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2024-07-31 14:42:25 +0200

    WIP diary date

--- app/src/main/java/com/example/dailymacros/ui/screens/diary/DiaryScreen.kt
+++ app/src/main/java/com/example/dailymacros/ui/screens/diary/DiaryScreen.kt
@@ -1,19 +1,97 @@
 package com.example.dailymacros.ui.screens.diary
 
+import android.text.format.DateUtils
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerFormatter
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.dailymacros.ui.composables.DMTopAppBar
+import java.time.Instant
+import java.util.Date
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Diary(navController: NavHostController) {
     Scaffold (
         topBar = { DMTopAppBar(navController) }
     ){paddingValues ->
-        Text("Diary", modifier = Modifier.padding(paddingValues))
+        //val dateState = rememberDatePickerState(initialDisplayMode = DisplayMode.Input)
+
+        //DatePicker(state = dateState, modifier = Modifier.padding(16.dp), dateFormatter = DatePickerFormatter())
+        DatePickerWithDialog(Modifier.padding(paddingValues))
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerWithDialog(
+    modifier: Modifier = Modifier
+) {
+    val dateState = rememberDatePickerState()
+    Log.v("DiaryScreen", dateState.selectedDateMillis.toString())
+    var showDialog by remember { mutableStateOf(false) }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = dateState.selectedDateMillis.toString(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable(onClick = {
+                    showDialog = true
+                }),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.headlineMedium
+        )
+        if (showDialog) {
+            DatePickerDialog(
+                onDismissRequest = { showDialog = false },
+                confirmButton = {
+                    Button(
+                        onClick = { showDialog = false }
+                    ) {
+                        Text(text = "OK")
+                    }
+                },
+                dismissButton = {
+                    Button(
+                        onClick = { showDialog = false }
+                    ) {
+                        Text(text = "Cancel")
+                    }
+                }
+            ) {
+                DatePicker(
+                    state = dateState,
+                    showModeToggle = true
+                )
+            }
+        }
+    }
+}
