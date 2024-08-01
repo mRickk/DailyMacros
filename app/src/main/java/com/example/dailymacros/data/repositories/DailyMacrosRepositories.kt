898aa6216ecd707a2ddf3774d54b723a5f5556f3
Matteo Sartini <matteo.sartini26@gmail.com>
2024-08-01 16:27:34 +0200

    added user database interface (to be tested)

--- app/src/main/java/com/example/dailymacros/data/repositories/DailyMacrosRepositories.kt
+++ app/src/main/java/com/example/dailymacros/data/repositories/DailyMacrosRepositories.kt
@@ -1,2 +1,17 @@
 package com.example.dailymacros.data.repositories
 
+import com.example.dailymacros.data.database.User
+import com.example.dailymacros.data.database.UserDAO
+import kotlinx.coroutines.flow.Flow
+
+class PokExploreRepository(
+    private val userDAO: UserDAO,
+) {
+
+    val users: Flow<List<User>> = userDAO.getAllUsers()
+
+    /* User */
+    suspend fun insertUser(user: User) = userDAO.insert(user)
+
+    suspend fun login(email: String, password: String) = userDAO.login(email, password)
+}
