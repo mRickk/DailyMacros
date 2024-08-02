e5d921b4172acb7d534ebc99a26f021275404dd3
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2024-08-02 18:42:26 +0200

    Added some entities to db

--- app/src/main/java/com/example/dailymacros/data/database/DailyMacrosDatabase.kt
+++ app/src/main/java/com/example/dailymacros/data/database/DailyMacrosDatabase.kt
@@ -3,8 +3,13 @@ package com.example.dailymacros.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.dailymacros.utilities.DateConverter
 
-@Database(entities = [User::class], version = 8)
+@Database(entities = [User::class, Food::class, Meal::class, FoodInsideMeal::class], version = 8)
+@TypeConverters(DateConverter::class)
 abstract class DailyMacrosDatabase : RoomDatabase() {
     abstract fun userDAO(): UserDAO
+    abstract fun foodDAO(): FoodDAO
+    abstract fun mealDAO(): MealDAO
+    abstract fun foodInsideMealDAO(): FoodInsideMealDAO
 }
---
+++ app/src/main/java/com/example/dailymacros/utilities/ListConverter.kt
@@ -0,0 +1,25 @@
+package com.example.dailymacros.utilities
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+inline fun <reified T> Gson.fromJson(json: String) =
+    fromJson<T>(json, object : TypeToken<T>() {}.type)
+
+class ListConverter {
+
+    @TypeConverter
+    fun fromStringArrayList(value: List<String>): String {
+        return Gson().toJson(value)
+    }
+
+    @TypeConverter
+    fun toStringArrayList(value: String): List<String> {
+        return try {
+            Gson().fromJson<List<String>>(value) //using extension function
+        } catch (e: Exception) {
+            arrayListOf()
+        }
+    }
+}
