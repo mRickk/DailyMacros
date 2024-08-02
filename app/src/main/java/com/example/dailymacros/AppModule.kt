23013ba57fdbab77b0288c6a2a21e9cb225aecb4
Matteo Sartini <matteo.sartini26@gmail.com>
2024-08-02 16:44:16 +0200

    fixed appmodule dependencies for database

--- app/src/main/java/com/example/dailymacros/AppModule.kt
+++ app/src/main/java/com/example/dailymacros/AppModule.kt
@@ -2,7 +2,11 @@ package com.example.dailymacros
 
 import android.content.Context
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.room.Room
+import com.example.dailymacros.data.database.DailyMacrosDatabase
+import com.example.dailymacros.data.repositories.DailyMacrosRepository
 import com.example.dailymacros.data.repositories.ThemeRepository
+import com.example.dailymacros.ui.screens.login.LoginViewModel
 import com.example.dailymacros.ui.screens.settings.SettingsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -10,7 +14,19 @@ import org.koin.dsl.module
 val Context.dataStore
         by preferencesDataStore("theme")
 val appModule = module {
+
+    single {
+        Room.databaseBuilder(
+            get(),
+            DailyMacrosDatabase::class.java,
+            "dailymacros"
+        ).fallbackToDestructiveMigration().build()
+    }
+
     single { get<Context>().dataStore }
     single { ThemeRepository(get()) }
+    single { DailyMacrosRepository(
+        get<DailyMacrosDatabase>().userDAO()) }
     viewModel { SettingsViewModel(get()) }
+    viewModel { LoginViewModel(get()) }
 }
