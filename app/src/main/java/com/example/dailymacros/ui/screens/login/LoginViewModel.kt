c3951048289b0c8ccf7beebe9f958c16d4775eba
Matteo Sartini <matteo.sartini26@gmail.com>
2024-08-15 16:02:27 +0200

    implemented login viewmodel

--- app/src/main/java/com/example/dailymacros/ui/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/example/dailymacros/ui/screens/login/LoginViewModel.kt
@@ -4,17 +4,40 @@ import android.content.IntentSender.OnFinished
 import android.provider.ContactsContract.CommonDataKinds.Email
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.dailymacros.data.database.User
 import com.example.dailymacros.data.repositories.DailyMacrosRepository
+import com.example.dailymacros.data.repositories.DatastoreRepository
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 interface LoginActions {
     fun setUser(user: User): Job
-    fun login(email: Email, password: String, onFinished: () -> Unit = {}): Job
+    fun login(email: String, password: String, onFinished: () -> Unit = {}): Job
 }
 
 class LoginViewModel(
-    private val dailyMacrosRepository: DailyMacrosRepository
+    private val dailyMacrosRepository: DailyMacrosRepository,
+    private val datastoreRepository: DatastoreRepository
 ) : ViewModel() {
-    // ViewModel implementation
+    var loggedUser = mutableStateOf<User?>(null)
+        private set
+
+    val actions = object : LoginActions {
+        override fun setUser(user: User) = viewModelScope.launch {
+            datastoreRepository.saveUser(user)
+        }
+
+        override fun login(email: String, password: String, onFinished: () -> Unit) = viewModelScope.launch {
+            loggedUser = mutableStateOf(dailyMacrosRepository.login(email, password))
+            onFinished()
+        }
+    }
+
+    init {
+        viewModelScope.launch {
+            loggedUser = mutableStateOf(datastoreRepository.user.first())
+        }
+    }
 }
