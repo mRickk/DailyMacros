3de154909cce4fc32d9f7957352545143b4873b6
Matteo Sartini <matteo.sartini26@gmail.com>
2024-08-26 14:26:32 +0200

    Added dietViewModel references

--- app/src/main/java/com/example/dailymacros/AppModule.kt
+++ app/src/main/java/com/example/dailymacros/AppModule.kt
@@ -10,6 +10,7 @@ import com.example.dailymacros.data.repositories.ThemeRepository
 import com.example.dailymacros.ui.screens.addexercise.AddExerciseViewModel
 import com.example.dailymacros.ui.screens.addfood.AddFoodViewModel
 import com.example.dailymacros.ui.screens.diary.DiaryViewModel
+import com.example.dailymacros.ui.screens.diet.DietViewModel
 import com.example.dailymacros.ui.screens.editprofile.EditProfileViewModel
 import com.example.dailymacros.ui.screens.login.LoginViewModel
 import com.example.dailymacros.ui.screens.overview.OverviewViewModel
@@ -55,4 +56,5 @@ val appModule = module {
     viewModel { SelectFoodViewModel(get()) }
     viewModel { AddFoodViewModel(get()) }
     viewModel {OverviewViewModel(get())}
+    viewModel {DietViewModel(get(), get())}
 }
