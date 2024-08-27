85f3883cba9546a23b5312dac64651e11b2d2cab
Matteo Sartini <matteo.sartini26@gmail.com>
2024-08-27 11:40:48 +0200

    Diet screen additional options

--- app/src/main/java/com/example/dailymacros/ui/screens/diet/DietScreen.kt
+++ app/src/main/java/com/example/dailymacros/ui/screens/diet/DietScreen.kt
@@ -1,13 +1,15 @@
 package com.example.dailymacros.ui.screens.diet
 
+import android.util.Log
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowForwardIos
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -15,6 +17,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.wear.compose.material.ContentAlpha
+import com.example.dailymacros.data.database.ActivityType
+import com.example.dailymacros.data.database.DietType
+import com.example.dailymacros.data.database.GoalType
 import com.example.dailymacros.ui.composables.DMTopAppBar
 import com.example.dailymacros.ui.composables.NavBar
 import com.example.dailymacros.ui.composables.PieChart
@@ -23,12 +28,24 @@ import com.example.dailymacros.ui.theme.Fat
 import com.example.dailymacros.ui.theme.Protein
 
 @Composable
-fun Diet(navController: NavHostController, dietScreenViewModel: DietViewModel) {
+fun Diet(navController: NavHostController, dietViewModel: DietViewModel) {
+    dietViewModel.actions.getUser()
+    var user = dietViewModel.loggedUser.user
+    var diet by remember { mutableStateOf(DietType.STANDARD) }
+    var goal by remember { mutableStateOf(GoalType.MAINTAIN_WEIGHT) }
+    var activity by remember { mutableStateOf(ActivityType.SEDENTARY) }
 
-    val user = dietScreenViewModel.loggedUser.user
+    fun updateValues() {
+        diet = user!!.diet
+        goal = user!!.goal
+        activity = user!!.activity
+    }
 
-
-
+    LaunchedEffect(user) {
+        if (user != null) {
+            updateValues()
+        }
+    }
 
     Scaffold(
         topBar = { DMTopAppBar(navController) },
@@ -45,7 +62,7 @@ fun Diet(navController: NavHostController, dietScreenViewModel: DietViewModel) {
             item {
                 // Section: Total Calorie Needs
                 Text(
-                    text = "Total calorie needs",
+                    text = "Total caloric needs",
                     fontSize = 24.sp,
                     fontWeight = FontWeight.Bold
                 )
@@ -58,7 +75,7 @@ fun Diet(navController: NavHostController, dietScreenViewModel: DietViewModel) {
                     contentAlignment = Alignment.Center
                 ) {
                     Text(
-                        text = "${user?.dailyKcal} kcal",
+                        text = "${user?.dailyKcal ?: "N/A"} kcal",
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Bold
                     )
@@ -135,68 +152,131 @@ fun Diet(navController: NavHostController, dietScreenViewModel: DietViewModel) {
                     fontWeight = FontWeight.Bold
                 )
                 Spacer(modifier = Modifier.height(16.dp)) // Spacer between title and boxes
-                // Column for clickable boxes
+                // Column for dropdown menus
                 Column(
                     modifier = Modifier.fillMaxWidth(),
                     verticalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
-                    // Type of Diet Box
+                    var expandedDiet by remember { mutableStateOf(false) }
+                    var expandedActivity by remember { mutableStateOf(false) }
+                    var expandedGoal by remember { mutableStateOf(false) }
+
+                    // Type of Diet Dropdown
                     Box(
                         modifier = Modifier
                             .fillMaxWidth()
                             .border(1.dp, MaterialTheme.colorScheme.onSurface.copy(alpha = ContentAlpha.disabled))
-                            .clickable { /* Handle click */ }
+                            .clickable { expandedDiet = true }
                             .padding(16.dp),
                         contentAlignment = Alignment.Center
                     ) {
                         Row(
                             modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Text(text = "Type of diet")
+                            Text(text = "Diet")
                             Row(verticalAlignment = Alignment.CenterVertically) {
-                                Text(text = "Value 1")
-                                Icon(imageVector = Icons.Default.ArrowForwardIos, contentDescription = null)
+                                Text(text = diet.string ?: "N/A")
+                                Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+                            }
+                        }
+                        DropdownMenu(
+                            expanded = expandedDiet,
+                            onDismissRequest = { expandedDiet = false },
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            DietType.entries.forEach { option ->
+                                DropdownMenuItem(
+                                    onClick = {
+                                        Log.v("DietScreen", "Option: $option")
+                                        expandedDiet = false
+                                        dietViewModel.actions.updateUser(option, dietViewModel.loggedUser.user!!.activity, dietViewModel.loggedUser.user!!.goal)
+                                        user = dietViewModel.loggedUser.user
+                                        updateValues()
+                                    },
+                                    text = { Text(text = option.string) }
+                                )
                             }
                         }
                     }
-                    // Activity Level Box
+
+                    // Activity Level Dropdown
                     Box(
                         modifier = Modifier
                             .fillMaxWidth()
                             .border(1.dp, MaterialTheme.colorScheme.onSurface.copy(alpha = ContentAlpha.disabled))
-                            .clickable { /* Handle click */ }
+                            .clickable { expandedActivity = true }
                             .padding(16.dp),
                         contentAlignment = Alignment.Center
                     ) {
                         Row(
                             modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
                             Text(text = "Activity level")
                             Row(verticalAlignment = Alignment.CenterVertically) {
-                                Text(text = "${user?.activity?.string}")
-                                Icon(imageVector = Icons.Default.ArrowForwardIos, contentDescription = null)
+                                Text(text = activity.string ?: "N/A")
+                                Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+                            }
+                        }
+                        DropdownMenu(
+                            expanded = expandedActivity,
+                            onDismissRequest = { expandedActivity = false },
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            ActivityType.entries.forEach { option ->
+                                DropdownMenuItem(
+                                    onClick = {
+                                        Log.v("DietScreen", "Option: $option")
+                                        expandedActivity = false
+                                        dietViewModel.actions.updateUser(dietViewModel.loggedUser.user!!.diet, option, dietViewModel.loggedUser.user!!.goal)
+                                        user = dietViewModel.loggedUser.user
+                                        updateValues()
+                                    },
+                                    text = { Text(text = option.string) }
+                                )
                             }
                         }
                     }
-                    // Obiettivo Box
+
+                    // Objective Dropdown
                     Box(
                         modifier = Modifier
                             .fillMaxWidth()
                             .border(1.dp, MaterialTheme.colorScheme.onSurface.copy(alpha = ContentAlpha.disabled))
-                            .clickable { /* Handle click */ }
+                            .clickable { expandedGoal = true }
                             .padding(16.dp),
                         contentAlignment = Alignment.Center
                     ) {
                         Row(
                             modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Text(text = "Objective")
+                            Text(text = "Goal")
                             Row(verticalAlignment = Alignment.CenterVertically) {
-                                Text(text = "${user?.goal?.string}")
-                                Icon(imageVector = Icons.Default.ArrowForwardIos, contentDescription = null)
+                                Text(text = goal.string ?: "N/A")
+                                Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+                            }
+                        }
+                        DropdownMenu(
+                            expanded = expandedGoal,
+                            onDismissRequest = { expandedGoal = false },
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            GoalType.entries.forEach { option ->
+                                DropdownMenuItem(
+                                    onClick = {
+                                        Log.v("DietScreen", "Option: $option")
+                                        expandedGoal = false
+                                        dietViewModel.actions.updateUser(dietViewModel.loggedUser.user!!.diet, dietViewModel.loggedUser.user!!.activity, option)
+                                        user = dietViewModel.loggedUser.user
+                                        updateValues()
+                                    },
+                                    text = { Text(text = option.string) }
+                                )
                             }
                         }
                     }
